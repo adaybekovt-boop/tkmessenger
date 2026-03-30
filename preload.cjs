6a8dababe5153090c1c324a60a39ce25f642b0e8
adaybekovt-boop <adaybekovt@bk.ru>
2026-03-30 23:00:49 +0500

    Add files via upload

---
+++ preload.cjs
@@ -0,0 +1,9 @@
+/**
+ * Electron preload: contextIsolation on, nodeIntegration off.
+ * Expose only what the renderer needs later via contextBridge (currently empty).
+ */
+const { contextBridge } = require('electron');
+
+contextBridge.exposeInMainWorld('orbitsElectron', {
+  platform: process.platform,
+});
