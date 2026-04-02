263998c5d90058be5347eb72501ab11741b76f32
adaybekovt-boop <adaybekovt@bk.ru>
2026-04-03 00:38:05 +0500

    new update

--- src/core/file.js
+++
@@ -1,4 +0,0 @@
-export async function fileSha256Buffer(arrayBuffer) {
-  const buf = await crypto.subtle.digest('SHA-256', arrayBuffer);
-  return Array.from(new Uint8Array(buf)).map(b => b.toString(16).padStart(2, '0')).join('');
-}
